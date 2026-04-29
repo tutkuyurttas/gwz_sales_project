1806ce8cb62911573959d2d39b7eb6ccd0d1f11c
Tutku YURTTAŞ <tutkuyurttass@gmail.com>
2026-04-30 00:29:04 +0300

    Update gwz_sales.sql
    add pofit

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,6 +1,8 @@
 SELECT
   date_date,
-  SUM(turnover) AS turnover
+  SUM(turnover) AS turnover,
+  SUM(purchase_price) AS purchase_cost,
+  SUM(turnover) - SUM(purchase_price) AS profit
 FROM `data-analytics-bootcamp-363212.course14.gwz_sales`
 GROUP BY date_date
 ORDER BY date_date
